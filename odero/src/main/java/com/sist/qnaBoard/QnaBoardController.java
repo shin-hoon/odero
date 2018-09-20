1a101e970bca00381ac424fcedbcdb2ab29b8ad8
duniv209_14 <duniv209_14@DESKTOP-B4KE7OU>
2018-09-20 11:45:59 +0900

    gg

--- odero/src/main/java/com/sist/qnaBoard/QnaBoardController.java
+++ odero/src/main/java/com/sist/qnaBoard/QnaBoardController.java
@@ -2,8 +2,6 @@ package com.sist.qnaBoard;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
